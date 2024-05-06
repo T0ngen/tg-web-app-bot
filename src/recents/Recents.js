81271240cf5ddae29eb9aa62a4a505e28f6a3127
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-05-06 19:34:41 +0500

    da

--- src/recents/Recents.js
+++ src/recents/Recents.js
@@ -27,7 +27,7 @@ class Recents  extends React.Component{
         stories={stories}
         defaultInterval={2000}
         width={'100%'}
-        height={768}
+        height={'100%'}
       />
     );
   
