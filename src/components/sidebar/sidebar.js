099b849c6ab35f01bb6cbf28a46dcce05dff8255
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-05-14 01:24:04 +0500

    da

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import logo from '../ImageLogo/refsystem.jpg';
+// import logo from '../ImageLogo/refsystem.jpg';
 import logo2 from '../ImageLogo/statya.jpg';
 import logo3 from '../ImageLogo/matherials.jpg';
 
